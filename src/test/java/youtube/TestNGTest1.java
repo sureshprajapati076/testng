54f4dedfb084524408caf58690a70c179b8532bc
sureshprajapati076 <sureshprajapati076@gmail.com>
2022-12-01 06:56:01 -0800

    window handle and handles testng method added

--- src/test/java/youtube/TestNGTest1.java
+++ src/test/java/youtube/TestNGTest1.java
@@ -1,10 +1,18 @@
 package youtube;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.time.LocalDateTime;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class TestNGTest1 {
 
@@ -35,6 +43,69 @@ public class TestNGTest1 {
 
 
 
+    @Test
+    public void windowHandle() throws Exception {
+
+        WebDriver driver = new ChromeDriver();
 
 
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+
+        WebElement twitter= driver.findElement(By.xpath("//a[@href='https://twitter.com/orangehrm?lang=en']"));
+        WebElement facebook= driver.findElement(By.xpath("//a[@href='https://www.facebook.com/OrangeHRM/']"));
+        WebElement linkedin= driver.findElement(By.xpath("//a[@href='https://www.linkedin.com/company/orangehrm/mycompany/']"));
+        WebElement youtubeIcon = driver.findElement(By.xpath("//a[@href='https://www.youtube.com/c/OrangeHRMInc']"));
+
+        waitForNSeconds(1);
+
+        twitter.click();
+
+        waitForNSeconds(1);
+
+        facebook.click();
+
+        waitForNSeconds(1);
+
+        linkedin.click();
+        waitForNSeconds(1);
+
+        youtubeIcon.click();
+        waitForNSeconds(1);
+
+
+        String currentwindow = driver.getWindowHandle(); //parent window
+        Set<String> allWindows = driver.getWindowHandles();
+//        waitForNSeconds(4);
+//        System.out.println("CURRENT WINDOW: " + driver.switchTo().window(currentwindow).getTitle());
+//
+//        allWindows.forEach(windowHandle-> {
+//            System.out.println("Another Window: "+ driver.switchTo().window(windowHandle).getTitle());
+//            waitForNSeconds(5);
+//        });
+
+        openWindowByTitle(driver,"youtube",allWindows);
+        waitForNSeconds(5);
+
+        openWindowByTitle(driver,"twitter",allWindows);
+        waitForNSeconds(5);
+        openWindowByTitle(driver,"youtube",allWindows);
+
+        waitForNSeconds(5);
+        openWindowByTitle(driver,"linkedin",allWindows);
+
+
+        waitForNSeconds(5);
+
+        driver.quit();
+    }
+
+    private void openWindowByTitle(WebDriver webDriver, String title, Set<String> allWindows) {
+
+        for(String windowHandleText: allWindows){
+            String titleFromHandle= webDriver.switchTo().window(windowHandleText).getTitle().toLowerCase();
+            if(titleFromHandle.contains(title.toLowerCase())) return;
+        }
+
+    }
 }
