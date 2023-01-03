8a68c51abb87578129b7aee86786b0aa22e70026
sureshprajapati076 <sureshprajapati076@gmail.com>
2023-01-03 12:30:34 -0800

    minor update

--- src/test/java/restassured/RandomApi.java
+++ src/test/java/restassured/RandomApi.java
@@ -15,7 +15,6 @@ public class RandomApi {
     }
 
 
-
     public class Result {
         public String gender;
         public Name name;
@@ -29,72 +28,72 @@ public class RandomApi {
         public Id id;
         public Picture picture;
         public String nat;
-
-        public class Coordinates {
-            public String latitude;
-            public String longitude;
-        }
-
-        public class Dob {
-            public String date;
-            public int age;
-        }
-
-        public class Id {
-            public String name;
-            public String value;
-        }
-
-
-
-        public class Location {
-            public Street street;
-            public String city;
-            public String state;
-            public String country;
-            public String postcode;
-            public Coordinates coordinates;
-            public Timezone timezone;
-        }
-
-        public class Login {
-            public String uuid;
-            public String username;
-            public String password;
-            public String salt;
-            public String md5;
-            public String sha1;
-            public String sha256;
-        }
-
-        public class Name {
-            public String title;
-            public String first;
-            public String last;
-        }
-
-        public class Picture {
-            public String large;
-            public String medium;
-            public String thumbnail;
-        }
-
-        public class Registered {
-            public String date;
-            public int age;
-        }
-
-
-        public class Street {
-            public int number;
-            public String name;
-        }
-
-        public class Timezone {
-            public String offset;
-            public String description;
-        }
     }
+
+    public class Coordinates {
+        public String latitude;
+        public String longitude;
+    }
+
+    public class Dob {
+        public String date;
+        public int age;
+    }
+
+    public class Id {
+        public String name;
+        public String value;
+    }
+
+
+    public class Location {
+        public Street street;
+        public String city;
+        public String state;
+        public String country;
+        public String postcode;
+        public Coordinates coordinates;
+        public Timezone timezone;
+    }
+
+    public class Login {
+        public String uuid;
+        public String username;
+        public String password;
+        public String salt;
+        public String md5;
+        public String sha1;
+        public String sha256;
+    }
+
+    public class Name {
+        public String title;
+        public String first;
+        public String last;
+    }
+
+    public class Picture {
+        public String large;
+        public String medium;
+        public String thumbnail;
+    }
+
+    public class Registered {
+        public String date;
+        public int age;
+    }
+
+
+    public class Street {
+        public int number;
+        public String name;
+    }
+
+    public class Timezone {
+        public String offset;
+        public String description;
+    }
+
 }
 
 
